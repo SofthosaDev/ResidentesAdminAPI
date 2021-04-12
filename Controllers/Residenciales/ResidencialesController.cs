5da1c3672d9f5b73be43706ce51c9eb83a7e8562
cesargroening@gmail.com <rbautista@masadelanto.com>
2021-04-12 10:13:28 -0500

    Agregué combo de residenciales

---
+++ Controllers/Residenciales/ResidencialesController.cs
@@ -0,0 +1,45 @@
+﻿using EvaluadorFinancieraWS.Services.Cobranza.Utilidades;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WsAdminResidentes.Models.Residencias;
+
+namespace WsAdminResidentes.Controllers.Residenciales
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class ResidencialesController : ControllerBase
+    {
+        private readonly IBaseDatosService _db;
+        public ResidencialesController(IBaseDatosService _db) {
+            this._db = _db;
+        }
+        [HttpGet]
+        public List<Residencia> ConsultarTodo(string Nombre = "") {
+           List<Residencia> res = _db.consultarSp<Residencia>("Residentes.SpResidencialCON", new { 
+                Nombre
+            });
+            return res;
+        }
+
+        [HttpGet("{IdResidencial}")]
+        public IActionResult ConsultarTodo([FromRoute] int IdResidencial)
+        {
+            Residencia res = _db.consultarSp<Residencia>("Residentes.SpResidencialCON", new
+            {
+                IdResidencial
+            }).FirstOrDefault();
+
+            if (res == null) {
+                return NotFound();
+            }
+            return Ok(res);
+        }
+
+    }
+}
---
+++ Models/Residencias/Residencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WsAdminResidentes.Models.Residencias
+{
+    public class Residencia
+    {
+        public int IdResidencial { get; set; }
+        public string Nombre { get; set; }
+        public int UbicacionAproximadaId { get; set; }
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
+        public int MunicipioId { get; set; }
+        public int EstadoId { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
